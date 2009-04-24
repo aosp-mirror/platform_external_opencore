463129886af7454a33574fdafa73fa2bb154a68d
niko <niko@google.com>
2009-04-24 10:56:46 -0700

    * pvmi/pvmf/src/pvmf_node_interface.cpp: 	Minor cleanup of the loops going over the sessions (uses standard 	type and uses ++i)
    	For the method not taking an event instance, build it and call the
    	method that takes an event instance to avoid duplicate code.
    
    	Moved the test for existance of the observer before we build the
    	copy of the event. There is no point doing that if the observer is
    	null.
    
            Added extra check to make sure the wrong type of event is not sent
    	to a handler.
    
    	* pvmi/pvmf/include/pvmf_node_interface.h:
    	Fixed some comments to be less < 80.
    
    	Removed the mention of TPVErrorIndicationType from the comments
    	since it is not part of the code base.
    
    	Made the input to the event handlers be *const* references.
    
    Patch Set 2: Fixed more comment > 80.
                 Reverted back the parameter's name event to aEvent
                 for consistency sake
    
    Patch Set 3: Per reviewer comment, added an extra temp
                 to cache the vector size value.
                 Added a todo to try to clean up the similarity between the
                 loops.

--- pvmi/pvmf/src/pvmf_node_interface.cpp
+++ pvmi/pvmf/src/pvmf_node_interface.cpp
@@ -18,6 +18,9 @@
 
 #include "pvmf_node_interface.h"
 
+// TODO: Both the info and error handling look very similar, we should
+// try to refactor them at some point (but beware of the info/error
+// context difference).
 
 OSCL_EXPORT_REF void PVMFNodeInterface::SetState(TPVMFNodeInterfaceState s)
 {
@@ -25,75 +28,101 @@ OSCL_EXPORT_REF void PVMFNodeInterface::SetState(TPVMFNodeInterfaceState s)
     ReportInfoEvent(PVMFInfoStateChanged, (OsclAny*)s);
 }
 
-OSCL_EXPORT_REF void PVMFNodeInterface::ReportCmdCompleteEvent(PVMFSessionId s, PVMFCmdResp &resp)
+OSCL_EXPORT_REF void PVMFNodeInterface::ReportCmdCompleteEvent(
+        PVMFSessionId session, const PVMFCmdResp& resp)
 {
-    for (uint32 i = 0;i < iSessions.size();i++)
+    const unsigned int size = iSessions.size();
+
+    for (unsigned int i = 0; i < size; ++i)
     {
-        if (iSessions[i].iId == s)
+        if (iSessions[i].iId == session)
         {
-            if (iSessions[i].iInfo.iCmdStatusObserver)
-                iSessions[i].iInfo.iCmdStatusObserver->NodeCommandCompleted(resp);
-            break;
+            PVMFNodeCmdStatusObserver *observer = iSessions[i].iInfo.iCmdStatusObserver;
+
+            if (NULL != observer)
+            {
+                observer->NodeCommandCompleted(resp);
+            }
+            return;
         }
     }
 }
 
-OSCL_EXPORT_REF void PVMFNodeInterface::ReportErrorEvent(PVMFEventType aEventType, OsclAny* aEventData, PVInterface*aExtMsg)
+OSCL_EXPORT_REF void PVMFNodeInterface::ReportErrorEvent(
+        PVMFEventType aEventType, OsclAny* aEventData, PVInterface*aExtMsg)
 {
-    for (uint32 i = 0;i < iSessions.size();i++)
+    ReportErrorEvent(PVMFAsyncEvent(PVMFErrorEvent,
+                                    aEventType,
+                                    NULL,  // Error Context will be set below.
+                                    aExtMsg,
+                                    aEventData));
+}
+
+OSCL_EXPORT_REF void PVMFNodeInterface::ReportErrorEvent(
+        const PVMFAsyncEvent& aEvent)
+{
+    if (aEvent.IsA() != PVMFErrorEvent)
     {
-        PVMFAsyncEvent resp(PVMFErrorEvent
-                            , aEventType
-                            , iSessions[i].iInfo.iErrorContext
-                            , aExtMsg
-                            , aEventData);
-        if (iSessions[i].iInfo.iErrorObserver)
-            iSessions[i].iInfo.iErrorObserver->HandleNodeErrorEvent(resp);
+        // TODO: Should log loudly an error here.
+        return;
+    }
+
+    const unsigned int size = iSessions.size();
+
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        PVMFNodeErrorEventObserver *observer = iSessions[i].iInfo.iErrorObserver;
+
+        if (NULL != observer)
+        {
+            PVMFAsyncEvent event(PVMFErrorEvent,
+                                 aEvent.GetEventType(),
+                                 iSessions[i].iInfo.iErrorContext,
+                                 aEvent.GetEventExtensionInterface(),
+                                 aEvent.GetEventData(),
+                                 aEvent.GetLocalBuffer(),
+                                 aEvent.GetLocalBufferSize());
+            observer->HandleNodeErrorEvent(event);
+        }
     }
 }
 
-OSCL_EXPORT_REF void PVMFNodeInterface::ReportErrorEvent(PVMFAsyncEvent &aEvent)
+OSCL_EXPORT_REF void PVMFNodeInterface::ReportInfoEvent(
+        PVMFEventType aEventType, OsclAny* aEventData, PVInterface*aExtMsg)
 {
-    for (uint32 i = 0;i < iSessions.size();i++)
-    {
-        PVMFAsyncEvent resp(PVMFErrorEvent
-                            , aEvent.GetEventType()
-                            , iSessions[i].iInfo.iErrorContext
-                            , aEvent.GetEventExtensionInterface()
-                            , aEvent.GetEventData()
-                            , aEvent.GetLocalBuffer()
-                            , aEvent.GetLocalBufferSize());
-        if (iSessions[i].iInfo.iErrorObserver)
-            iSessions[i].iInfo.iErrorObserver->HandleNodeErrorEvent(resp);
-    }
+    ReportInfoEvent(PVMFAsyncEvent(PVMFInfoEvent,
+                                   aEventType,
+                                   NULL,  // Info Context will be set below.
+                                   aExtMsg,
+                                   aEventData));
 }
 
-OSCL_EXPORT_REF void PVMFNodeInterface::ReportInfoEvent(PVMFEventType aEventType, OsclAny* aEventData, PVInterface*aExtMsg)
+OSCL_EXPORT_REF void PVMFNodeInterface::ReportInfoEvent(
+        const PVMFAsyncEvent& aEvent)
 {
-    for (uint32 i = 0;i < iSessions.size();i++)
+    if (aEvent.IsA() != PVMFInfoEvent)
     {
-        PVMFAsyncEvent resp(PVMFInfoEvent
-                            , aEventType
-                            , iSessions[i].iInfo.iInfoContext
-                            , aExtMsg
-                            , aEventData);
-        if (iSessions[i].iInfo.iInfoObserver)
-            iSessions[i].iInfo.iInfoObserver->HandleNodeInformationalEvent(resp);
+        // TODO: Should log loudly an error here.
+        return;
     }
-}
 
-OSCL_EXPORT_REF void PVMFNodeInterface::ReportInfoEvent(PVMFAsyncEvent &aEvent)
-{
-    for (uint32 i = 0;i < iSessions.size();i++)
+    const unsigned int size = iSessions.size();
+
+    for (unsigned int i = 0; i < size; ++i)
     {
-        PVMFAsyncEvent resp(PVMFInfoEvent
-                            , aEvent.GetEventType()
-                            , iSessions[i].iInfo.iInfoContext
-                            , aEvent.GetEventExtensionInterface()
-                            , aEvent.GetEventData()
-                            , aEvent.GetLocalBuffer()
-                            , aEvent.GetLocalBufferSize());
-        if (iSessions[i].iInfo.iInfoObserver)
-            iSessions[i].iInfo.iInfoObserver->HandleNodeInformationalEvent(resp);
+        PVMFNodeInfoEventObserver *observer = iSessions[i].iInfo.iInfoObserver;
+
+        if (NULL != observer)
+        {
+            PVMFAsyncEvent event(PVMFInfoEvent,
+                                 aEvent.GetEventType(),
+                                 iSessions[i].iInfo.iInfoContext,
+                                 aEvent.GetEventExtensionInterface(),
+                                 aEvent.GetEventData(),
+                                 aEvent.GetLocalBuffer(),
+                                 aEvent.GetLocalBufferSize());
+
+            observer->HandleNodeInformationalEvent(event);
+        }
     }
 }
