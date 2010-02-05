c1cb65dc5a2df9b0544f9100cd1bc155ea19db28
Ying Wang <wangying@google.com>
2010-02-04 17:22:28 -0800

    Fix intermittent Mac build errors.
    See http://b/issue?id=2391369

--- build_config/opencore_dynamic/Android_opencore_player.mk
+++ build_config/opencore_dynamic/Android_opencore_player.mk
@@ -24,6 +24,9 @@ LOCAL_WHOLE_STATIC_LIBRARIES := \
 
 LOCAL_WHOLE_STATIC_LIBRARIES +=  libandroidpv
 
+# to solve circular dependency among the static libraries.
+LOCAL_STATIC_LIBRARIES := $(LOCAL_STATIC_LIBRARIES) $(LOCAL_WHOLE_STATIC_LIBRARIES)
+
 LOCAL_MODULE := libopencore_player
 
 -include $(PV_TOP)/Android_platform_extras.mk
