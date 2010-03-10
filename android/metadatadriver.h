4c8d8c50f00467a21bbdaaad37b839b4af4b954e
Andreas Huber <andih@google.com>
2010-03-10 10:26:54 -0800

    Fix the mode constants in the metadatadriver to properly align with the public constants, this mismatch prevented proper metadata extraction.
    Change-Id: I1c1d541be8aebc7d45ec39891c53ba43fa946ece
    related-to-bug: 2074137

--- android/metadatadriver.h
+++ android/metadatadriver.h
@@ -122,8 +122,8 @@ private:
     };
 
     // We support get metadata, or get frame, or get both, or get neigther.
-    static const uint32 GET_METADATA_ONLY    = (0x01 << 0);
-    static const uint32 GET_FRAME_ONLY       = (0x01 << 1);
+    static const uint32 GET_METADATA_ONLY    = METADATA_MODE_METADATA_RETRIEVAL_ONLY;
+    static const uint32 GET_FRAME_ONLY       = METADATA_MODE_FRAME_CAPTURE_ONLY;
     static const uint32 MAX_VIDEO_FRAME_SIZE = 1280 * 720 * 4;  // Big enough?
     static const uint32 MAX_METADATA_STRING_LENGTH = 128;
     static const uint32 MAX_STRING_LENGTH = 512;
